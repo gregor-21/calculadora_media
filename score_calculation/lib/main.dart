63322e20ddd21d414d1a740156c7642af823e4c3
gregor_yuri <gregoryuri09@gmail.com>
2023-05-19 12:19:19 -0300

    atualizando

--- score_calculation/lib/main.dart
+++ score_calculation/lib/main.dart
@@ -2,5 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:score_calculation/calculator.dart';
 
 void main() {
-  runApp(MediaCalculatorApp());
+  //WidgetsFlutterBinding.ensureInitialized();
+  Future.delayed(Duration(seconds: 2), () {
+    runApp(MediaCalculatorApp());
+  });
 }
